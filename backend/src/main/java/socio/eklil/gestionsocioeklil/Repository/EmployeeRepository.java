3cb5fb078619cba3eba38418b09975b73ddb6a97
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-17 22:52:40 +0100

    Create EmployeeRepository.java

---
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Repository/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package socio.eklil.gestionsocioeklil.Repository;
+
+//import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import socio.eklil.gestionsocioeklil.Model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
+
+    Employee findByEmail(String email);
+    
+
+
+}
