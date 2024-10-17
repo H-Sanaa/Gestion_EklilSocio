cc069e90b7378d211591aabe441f75e889d1cb00
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-17 22:40:46 +0100

    Create GestionsocioeklilApplication.java

---
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/GestionsocioeklilApplication.java
@@ -0,0 +1,15 @@
+package socio.eklil.gestionsocioeklil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GestionsocioeklilApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionsocioeklilApplication.class, args);
+	}
+
+}
