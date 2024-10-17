4e8d3a62d9d91e2a78400efe5ee54703dce40675
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-17 22:56:24 +0100

    Create GestionsocioeklilApplicationTests.java

---
+++ backend/src/test/java/socio/eklil/gestionsocioeklil/GestionsocioeklilApplicationTests.java
@@ -0,0 +1,18 @@
+package socio.eklil.gestionsocioeklil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class GestionsocioeklilApplicationTests {
+	@Autowired
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
