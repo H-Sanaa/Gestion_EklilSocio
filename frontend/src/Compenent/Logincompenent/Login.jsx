d8653a3905ab9b4e5041dd6571dba4065549a42b
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-18 21:33:25 +0100

    Create Login.jsx

---
+++ frontend/src/Compenent/Logincompenent/Login.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Link, useNavigate,  } from "react-router-dom";
+
+
+import axios from "axios";
+import logo from "../Assets/logo.png"
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pdw_employee, setPassword] = useState("");
+  const navigate=useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
+    try {
+     const res=  await axios.post(`http://localhost:8080/api/login/${email}/${pdw_employee}`);
+        if(res.status===200){
+        
+          navigate("/dashboard");
+        }else{
+          alert("Email et mot de passe incorrecte");
+        }
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  return (
+
+    <div className="container">
+      <div className="row justify-content-center">
+         <div className="text-center">
+                
+             <img src={logo} alt="" width={150} height={100}/>
+             <h4 className="mt-1 mb-1 pb-1" > 
+                Centre Socio Culturel Tétouan     المركز السوسيو ثقافي بتطوان</h4> 
+                    <hr className="line" />
+            <h5 className="mt-1 mb-2 pb-1" >
+                Espace d'assistant administratif      فضاء مساعد اداري
+            </h5>
+          </div>
+          <br />
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-body">
+            
+              <h3 className="card-title text-center">Authentification</h3>
+              <br />
+           
+              <form onSubmit={handleLogin}>
+                <div className="form-group">
+                <i className="fas fa-user"></i>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <br />
+                <div className="form-group">
+                
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Mot de passe"
+                    value={pdw_employee}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <br />
+                <h6>vous n'etes pas encore enregistré?  <Link to="/register" >Crer un compte </Link></h6>
+              
+
+                <button id="button" className="btn  btn-block  col-md-12" type="submit">Connexion  </button>
+                      
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Login;
