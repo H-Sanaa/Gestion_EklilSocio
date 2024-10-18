47fc0a0f49655779ac13624c2dbc2308a21e3e0d
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-18 21:31:22 +0100

    Create Footer.jsx

---
+++ frontend/src/Compenent/HeaderFooter/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer style={{width:'100%',position:'fixed',bottom:'0',textAlign:'center',background:'black',color:'white'} }>
+           <span >&copy;2024 Développer par HALHOUL Sanaa. </span>
+
+    </footer>
+  )
+}
+
+export default Footer
