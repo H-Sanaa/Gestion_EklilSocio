e8fa94c98fa47ce7d92e25ec3f95d23797fefc6e
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-24 23:24:24 +0100

    Create Livre.jsx

---
+++ frontend/src/Compenent/LivreComp/Livre.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../HeaderFooter/Header';
+
+const Livre = () => {
+  return (
+    <>
+     <Header/>
+
+    
+
+
+
+
+    </>
+  )
+}
+
+export default Livre
