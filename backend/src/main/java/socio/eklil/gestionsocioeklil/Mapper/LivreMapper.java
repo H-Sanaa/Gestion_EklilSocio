f03acb95ce3547c35239407b3784057ae995308b
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-24 23:23:37 +0100

    Create LivreMapper.java

---
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Mapper/LivreMapper.java
@@ -0,0 +1,32 @@
+package socio.eklil.gestionsocioeklil.Mapper;
+
+import socio.eklil.gestionsocioeklil.Dto.LivreDto;
+import socio.eklil.gestionsocioeklil.Model.Livre;
+
+public class LivreMapper {
+    public static LivreDto
+    mapToLivreDto(Livre livre){
+        return new LivreDto(
+         livre.getId_livre(),
+         livre.getLivre_code(),
+         livre.getNom_auteur(),
+         livre.getNom_livre(),
+         livre.getPhoto()
+
+        );
+    }
+    public static Livre
+    mapToLivre(LivreDto livreDto){
+        return new Livre(
+            livreDto.getId_livre(),
+            livreDto.getLivre_code(),
+            livreDto.getNom_auteur(),
+            livreDto.getNom_livre(),
+            livreDto.getPhoto()
+
+
+        );
+    }
+
+
+}
