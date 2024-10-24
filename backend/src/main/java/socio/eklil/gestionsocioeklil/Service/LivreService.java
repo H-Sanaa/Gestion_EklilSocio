a924ffa662ee6fa89026cf1b0a441c3245f4822c
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-24 23:23:54 +0100

    Create LivreService.java

---
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Service/LivreService.java
@@ -0,0 +1,13 @@
+package socio.eklil.gestionsocioeklil.Service;
+
+import socio.eklil.gestionsocioeklil.Dto.LivreDto;
+
+public interface LivreService {
+
+    //create a new book(livre)
+    LivreDto createLivre(LivreDto livreDto);
+
+    
+
+
+}
