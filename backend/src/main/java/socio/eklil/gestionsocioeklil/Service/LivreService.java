85e08a606255acfbb3f77eb755a9e17354329064
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-25 21:39:55 +0100

    Update LivreService.java

--- backend/src/main/java/socio/eklil/gestionsocioeklil/Service/LivreService.java
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Service/LivreService.java
@@ -1,11 +1,16 @@
 package socio.eklil.gestionsocioeklil.Service;
 
+import java.util.List;
+
 import socio.eklil.gestionsocioeklil.Dto.LivreDto;
 
 public interface LivreService {
 
     //create a new book(livre)
     LivreDto createLivre(LivreDto livreDto);
+
+    //get a list of all the books
+    List<LivreDto> getAllLivre();
 
     
 
