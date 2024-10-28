fdc516dca760743169dd7d4849d5db8c34816e1c
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-28 22:46:49 +0100

    Update LivreController.java

--- backend/src/main/java/socio/eklil/gestionsocioeklil/Controller/LivreController.java
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Controller/LivreController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,6 +38,30 @@ public class LivreController {
         List<LivreDto> livres=livreService.getAllLivre();
         return ResponseEntity.ok(livres);
 
+    }
+
+    //REST API get 
+    @GetMapping("{id}")
+    public ResponseEntity<LivreDto>getLivreById(@PathVariable("id") Long livreId){
+        LivreDto livreDto=livreService.getLivreById(livreId);
+        return ResponseEntity.ok(livreDto);
+    }
+
+    //Build update book REST API
+    @PutMapping("{id}")
+    public ResponseEntity<LivreDto>updateLivre(
+        @PathVariable("id") Long livreId,
+        @RequestBody LivreDto updateLivre){
+        LivreDto livreDto=livreService.updateLivre(livreId, updateLivre);
+        return ResponseEntity.ok(livreDto);
+    }
+
+    //Build delete book REST API
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteLivre(
+        @PathVariable("id") Long livreId){
+    livreService.deleteLivre(livreId);
+    return ResponseEntity.ok("Le livre bien Supprimer");
     }
 
 
