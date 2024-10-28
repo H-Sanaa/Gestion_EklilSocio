31b51489093b4fc2c2334fe6f9d8eaa12b453f41
Sanaa01-halhoul <sanaahal01@gmail.com>
2024-10-28 22:47:32 +0100

    Update LivreServiceImp.java

--- backend/src/main/java/socio/eklil/gestionsocioeklil/Service/LivreServiceImp.java
+++ backend/src/main/java/socio/eklil/gestionsocioeklil/Service/LivreServiceImp.java
@@ -1,5 +1,6 @@
 package socio.eklil.gestionsocioeklil.Service;
 
+import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Service;
 import socio.eklil.gestionsocioeklil.Dto.LivreDto;
 import socio.eklil.gestionsocioeklil.Mapper.LivreMapper;
 import socio.eklil.gestionsocioeklil.Model.Livre;
+import socio.eklil.gestionsocioeklil.Exception.ResourceNotFoundException;
 import socio.eklil.gestionsocioeklil.Repository.LivreRepository;
 
 
@@ -30,5 +32,42 @@ public class LivreServiceImp implements LivreService  {
      List<Livre> livres=livreRepository.findAll();
      return livres.stream().map((livre)->LivreMapper.mapToLivreDto(livre)).collect(Collectors.toList());
     }
+
+    @Override
+    public LivreDto getLivreById(Long livreId) {
+      Livre livre=livreRepository.findById(livreId).orElseThrow(()-> new ResourceNotFoundException("Participant n'est pas reconnu avec id= "+livreId));
+      return LivreMapper.mapToLivreDto(livre);
+      
+    }
+
+    @Override
+    public LivreDto updateLivre(Long livreId, LivreDto updateLivre) {
+      Livre livre=livreRepository.findById(livreId).orElseThrow(()->new ResourceNotFoundException("Livre n'est pas existe avec id="+livreId));
+      livre.setNom_livre(updateLivre.getNom_livre());
+      livre.setNom_auteur(updateLivre.getNom_auteur());
+      livre.setAnee(updateLivre.getAnnee());
+      livre.setPrix(null);
+      livre.setLivre_code(updateLivre.getLivre_code());
+        // Convert photo from String to byte[] if it's Base64 encoded
+    if (updateLivre.getPhoto() != null) {
+      byte[] photoBytes = Base64.getDecoder().decode(updateLivre.getPhoto());
+      livre.setPhoto(photoBytes);
+  }
+      Livre updaLivreObj=livreRepository.save(livre);
+
+
+
+
+      return LivreMapper.mapToLivreDto(updaLivreObj);
+    }
+
+    @Override
+    public void deleteLivre(Long livreId) {
+      @SuppressWarnings("unused")
+      Livre livre=livreRepository.findById(livreId).orElseThrow(()-> new ResourceNotFoundException("Livre n'est pas existe avec id="+livreId));
+      livreRepository.deleteById(livreId);
+    }
+
+   
 
 }
